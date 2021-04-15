1470d6bc563f4fb4833b6a5c3d1f169faa8c3fa1
saurabh kumar <saurabh322001raj@gmail.com>
2021-04-15 14:24:07 +0530

    Day 1: learn about variable,constant,datatype

---
+++ fbcss.js
@@ -0,0 +1,21 @@
+// practical 1st
+// const markWeight =95;
+// const markHeight =1.88;
+// const johnWeight =85;
+// const johnHeight =1.76;
+// const markBmi=markWeight / markHeight**2;
+// const johnBmi= johnWeight/(johnHeight*johnHeight);
+// const markHeigerBmi = markBmi >johnBmi;
+// console.log(markHeigerBmi);
+// console.log(markBmi,johnBmi);
+// // console.log(johnBmi);
+const firstName ="saurabh";
+const year =2021;
+const birthDate =2000;
+const job="student";
+const saurabh = "I'm "+firstName + ",a " +(birthDate-year)+" old " +job + "!";
+console.log(saurabh);
+console.log("first name is saurabh and \n\ is sachin is brother");
+const saurabhNew = `i'm ${firstName} ,a
+ ${birthDate-year} old ${job}!`;
+console.log(saurabhNew);
