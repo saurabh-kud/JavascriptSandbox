9aa6fe14da5e8c730f36dd442ecbb51a41eccd16
saurabh kumar <saurabh322001raj@gmail.com>
2021-04-18 21:20:47 +0530

    Day 2:learn template literals,if_else

--- fbcss.js
+++ fbcss.js
@@ -8,14 +8,32 @@
 // const markHeigerBmi = markBmi >johnBmi;
 // console.log(markHeigerBmi);
 // console.log(markBmi,johnBmi);
-// // console.log(johnBmi);
-const firstName ="saurabh";
-const year =2021;
-const birthDate =2000;
-const job="student";
-const saurabh = "I'm "+firstName + ",a " +(birthDate-year)+" old " +job + "!";
-console.log(saurabh);
-console.log("first name is saurabh and \n\ is sachin is brother");
-const saurabhNew = `i'm ${firstName} ,a
- ${birthDate-year} old ${job}!`;
-console.log(saurabhNew);
+// console.log(johnBmi);
+// const firstName ="saurabh";
+// const year =2021;
+// const birthDate =2000;
+// const job="student";
+// const saurabh = "I'm "+firstName + ",a " +(birthDate-year)+" old " +job + "!";
+// console.log(saurabh);
+// console.log("first name is saurabh and \n\ is sachin is brother");
+// const saurabhNew = `i'm ${firstName} ,a
+//  ${birthDate-year} old ${job}!`;
+// console.log(saurabhNew);
+//quiz 2nd
+const markWeight =95;
+const markHeight =1.88;
+const johnWeight =85;
+const johnHeight =1.76;
+const markBmi=markWeight / markHeight**2;
+const johnBmi= johnWeight/(johnHeight*johnHeight);
+const markHeigerBmi = markBmi >johnBmi;
+console.log(markHeigerBmi);
+console.log(markBmi,johnBmi);
+if(markBmi > johnBmi){
+ console.log(`mark's BMI(${markBmi})is higher than john's!(${johnBmi})`);  
+
+}
+else{
+    console.log(`john's BMI(${johnBmi}) is higher than mark's!(${markBmi})`);
+}
+ 
